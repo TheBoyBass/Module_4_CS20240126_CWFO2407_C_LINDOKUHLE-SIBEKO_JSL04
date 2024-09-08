3498a1fd9755ca9ad47d65160770f51bdeb52637
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-09-09 01:49:16 +0200

    Added If condition to challenge 1

--- index.js
+++ index.js
@@ -4,15 +4,22 @@
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
-let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+let userPreference = '' // Possible values: 'Regular', 'Sugar-free', or undefined
+if (userPreference=='Sugar-free'){
+    console.log(`This User prefers the Sugar-free variety`);
+}else if (userPreference=='' || userPreference=='Regualar'){
+    console.log('This user prefers the Regular variety')
+}else{
+    console.log('This user prefers an unknown variety')
+}
+
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+console.log();
 
 
 // #### Challenge 3: Workout Intensity
